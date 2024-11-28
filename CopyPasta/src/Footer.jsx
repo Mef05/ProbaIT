cad7933ca3ce99a1759c07427d359280f408a90d
stef <andy05stef@gmail.com>
2024-11-28 02:05:10 +0200

    navbar v1

---
+++ CopyPasta/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <footer>
+                <p>© {new Date().getFullYear()} CopyPasta</p>
+            </footer>
+        </footer>
+    );
+}
+
+export default Footer;
